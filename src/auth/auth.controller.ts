9ede1edb7a46671786a0e633b8b4211857285027
maheechy77 <maheechy96@gmail.com>
2022-07-04 22:58:47 +0600

    movie data change

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -23,14 +23,14 @@ export class AuthController {
         {
           title: 'Spider-Man: No Way Home',
           releaseDate: 'December 17, 2021',
-          imageUrl: '/image/spider-man.jpg',
+          imageUrl: 'https://www.marvel.com/movies/spider-man-no-way-home',
           description:
             "Movie Description: Peter Parker seeks Doctor Strange's help to makepeople forget about Spiderman's identity. However, when the spell hecasts gets corrupted, several unwanted guests enter their universe.",
         },
         {
           title: 'Top Gun: Maverick',
           releaseDate: 'May 27, 2022',
-          imageUrl: '/image/topgun.jpg',
+          imageUrl: 'https://en.wikipedia.org/wiki/Top_Gun:_Maverick',
           description:
             " After more than 30 years of service as one of the Navy's top aviators, Pete Maverick Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him. Training a detachment of graduates for a special assignment, Maverick must confront the ghosts of his past and his deepest fears, culminating in a mission that demands the ultimate sacrifice from those who choose to fly it.",
         },
